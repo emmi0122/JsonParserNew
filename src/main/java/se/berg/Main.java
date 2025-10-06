70b583e06971c26810fd3249b9358af7fcc9b62e
Emilia Jarleback <emilia.jarleback@bergpropulsion.com>
2025-10-06 10:18:30 +0200

    Adding necessary components

--- src/main/java/se/berg/Main.java
+++ src/main/java/se/berg/Main.java
@@ -1,7 +1,10 @@
 package se.berg;
 
+import se.berg.domain.TestSequence;
+
 public class Main {
     public static void main(String[] args) {
-        
+        //Parse JSON file
+        //TestSequence seq = JsonParser.parseTestSequence("01_ENGINGE_SAFETY_SIGNLE_800M.JSON.json");
     }
 }
---
+++ src/main/java/se/berg/domain/TestSequence.java
@@ -0,0 +1,13 @@
+package se.berg.domain;
+
+public class TestSequence {
+    private String name;
+
+    public TestSequence(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
