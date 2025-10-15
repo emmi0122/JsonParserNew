ac365de1b5214c885b9e8af0cf54ffad35e8bee6
Emilia Jarleback <emilia.jarleback@bergpropulsion.com>
2025-10-15 14:03:50 +0200

    Adding some more test sequences and json files

--- src/main/java/se/berg/Main.java
+++ src/main/java/se/berg/Main.java
@@ -15,7 +15,10 @@ public class Main {
             //TestSequence seq = JsonParser.parseTestSequence("01_ENGINGE_SAFETY_SIGNLE_800M.JSON.json");
             //TestSequence seq = JsonParser.parseTestSequence("01_FIRST_ENGINE_RULES_SINGLE_800M.json");
             //TestSequence seq = JsonParser.parseTestSequence("01_ALARMS_AND_FAILURE_HANDLING_800M.json");
-            TestSequence seq = JsonParser.parseTestSequence("01_COMMAND_TRANSFER_800M.json");
+            //TestSequence seq = JsonParser.parseTestSequence("01_COMMAND_TRANSFER_800M.json");
+            //TestSequence seq = JsonParser.parseTestSequence("01_LOAD_CONTROL_SINGLE_800M.json");
+            //TestSequence seq = JsonParser.parseTestSequence("01_SHAFT_GENERATOR_OPERATION_SINGLE_800M.json");
+            TestSequence seq = JsonParser.parseTestSequence("01_DP_OPERATION_SINGLE_800M.json");
             Files.writeString(Paths.get("parsed_json.txt"), seq.toString());
         } catch (Exception e) {
             e.printStackTrace();
