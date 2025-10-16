75cc7109b7e76c3dd435e0fac6ae6ebaff2afa97
Emilia Jarleback <emilia.jarleback@bergpropulsion.com>
2025-10-16 13:02:51 +0200

    Adding so the json file is read from terminal instead of in Main method

--- src/main/java/se/berg/Main.java
+++ src/main/java/se/berg/Main.java
@@ -11,23 +11,17 @@ import se.berg.io.JsonParser;
  */
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Please specify a JSON file as an argument");
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+
         try {
-            TestSequence seq = JsonParser.parseTestSequence("01_ENGINGE_SAFETY_SIGNLE_800M.JSON.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_FIRST_ENGINE_RULES_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_ALARMS_AND_FAILURE_HANDLING_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_COMMAND_TRANSFER_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_LOAD_CONTROL_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_SHAFT_GENERATOR_OPERATION_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_DP_OPERATION_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("15_ENGINE_START_STOP_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_FIFI_OPERATION_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("01_SHAFT_BRAKE_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("26_OPERATING_MODE_FIX_PITCH_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("00_ENG_NO_HANDSHAKE_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("19_PTH_START_STOP_AND_MAIN_CLUTCH_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("20_GUI_TEST_NO_SHUTDOWN_GOVERNOR_HEALTHY_SINGLE_800M.json");
-            //TestSequence seq = JsonParser.parseTestSequence("22_DD_SINGLE_800M.json");
+            TestSequence seq = JsonParser.parseTestSequence(filePath);
             Files.writeString(Paths.get("parsed_json.txt"), seq.toString());
+            System.out.println("Parsing done. Result can be seen in parsed_json.txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
